bd0d971d9156d4df9d235e2b842b904e5e199de4
Ezzenix <anton.lampinen@gmail.com>
2023-07-21 16:29:23 +0200

    Initial commit

---
+++ src/main/java/ezzenix/chatanimation/ChatAnimation.java
@@ -0,0 +1,20 @@
+package ezzenix.chatanimation;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ChatAnimation implements ModInitializer {
+	public static final String MOD_ID = "chatanimation";
+    public static final Logger LOGGER = LoggerFactory.getLogger("chatanimation");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/ezzenix/chatanimation/mixin/ChatMixin.java
@@ -0,0 +1,149 @@
+package ezzenix.chatanimation.mixin;
+
+import com.google.common.collect.Lists;
+import com.mojang.logging.LogUtils;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.client.gui.hud.ChatHudLine;
+import net.minecraft.client.gui.hud.MessageIndicator;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.math.MathHelper;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Environment(EnvType.CLIENT)
+@Mixin(ChatHud.class)
+public class ChatMixin {
+	@Shadow private int scrolledLines;
+	@Shadow @Final private List<ChatHudLine.Visible> visibleMessages;
+	@Shadow private boolean hasUnreadNewMessages;
+	@Shadow @Final private MinecraftClient client;
+	@Shadow @Final private static final Logger LOGGER = LogUtils.getLogger();
+
+	@Shadow private void drawIndicatorIcon(DrawContext context, int x, int y, MessageIndicator.Icon icon) { }
+	@Shadow private int getIndicatorX(ChatHudLine.Visible line) { return 0; }
+	@Shadow private static double getMessageOpacityMultiplier(int age) { return 0; }
+	@Shadow private boolean isChatHidden() { return false; }
+	@Shadow private int getLineHeight() { return 0; }
+	@Shadow private boolean isChatFocused() { return false; }
+	@Shadow private double toChatLineX(double x) { return 0; }
+	@Shadow private double toChatLineY(double y) { return 0; }
+	@Shadow private int getMessageIndex(double chatLineX, double chatLineY) { return 0; }
+	@Shadow public int getVisibleLineCount() { return 0; }
+	@Shadow public double getChatScale() {
+		return 0;
+	}
+	@Shadow public int getWidth() { return 0; }
+
+	@Unique private final float fadeOffsetYScale = 0.6f; // scale * lineHeight
+	@Unique private final float fadeTime = 3f; // ticks
+
+	@Inject(method = "render", at = @At("HEAD"), cancellable = true)
+	public void render(DrawContext context, int currentTick, int mouseX, int mouseY, CallbackInfo ci) {
+		int y;
+		int w;
+		int textBackgroundOpacityFinal;
+		int chatOpacityFinal;
+		int ticksAlive;
+		if (this.isChatHidden()) {
+			return;
+		}
+		int visibleLineCount = this.getVisibleLineCount();
+		int visibleMessagesCount = this.visibleMessages.size();
+		if (visibleMessagesCount == 0) {
+			return;
+		}
+		boolean isFocused = this.isChatFocused();
+		float chatScale = (float)this.getChatScale();
+		int k = MathHelper.ceil((float)this.getWidth() / chatScale);
+		int scaledWindowHeight = context.getScaledWindowHeight();
+		context.getMatrices().push();
+		context.getMatrices().scale(chatScale, chatScale, 1.0f);
+		context.getMatrices().translate(4.0f, 0.0f, 0.0f);
+		int chatBottomY = MathHelper.floor((float)(scaledWindowHeight - 40) / chatScale);
+		int n = this.getMessageIndex(this.toChatLineX(mouseX), this.toChatLineY(mouseY));
+		double chatOpacity = this.client.options.getChatOpacity().getValue() * (double)0.9f + (double)0.1f;
+		double textBackgroundOpacity = this.client.options.getTextBackgroundOpacity().getValue();
+		double chatLineSpacing = this.client.options.getChatLineSpacing().getValue();
+		int lineHeight = this.getLineHeight();
+		int lineSpacing = (int)Math.round(-8.0 * (chatLineSpacing + 1.0) + 4.0 * chatLineSpacing);
+		int index = 0;
+		int chatDisplacementY = 0;
+		for (int r = 0; r + this.scrolledLines < this.visibleMessages.size() && r < visibleLineCount; ++r) {
+			int s = r + this.scrolledLines;
+			ChatHudLine.Visible visible = this.visibleMessages.get(s);
+			if (visible == null || (ticksAlive = currentTick - visible.addedTime()) >= 200 && !isFocused) continue;
+			if (r == 0 && ticksAlive < fadeTime) {
+				float maxDisplacement = (float)lineHeight*fadeOffsetYScale;
+				chatDisplacementY = (int)(maxDisplacement - (((float)ticksAlive/fadeTime)*maxDisplacement));
+			}
+			double opacity = isFocused ? 1.0 : getMessageOpacityMultiplier(ticksAlive);
+			if (ticksAlive < fadeTime) {
+				opacity = opacity * MathHelper.clamp(ticksAlive/fadeTime, 0, 1);
+			}
+			chatOpacityFinal = (int)(255.0 * opacity * chatOpacity);
+			textBackgroundOpacityFinal = (int)(255.0 * opacity * textBackgroundOpacity);
+			++index;
+			if (chatOpacityFinal <= 3) continue;
+			y = chatBottomY - r * lineHeight + chatDisplacementY;
+			context.getMatrices().push();
+			context.getMatrices().translate(0.0f, 0.0f, 50.0f);
+			context.fill(-4, y - lineHeight, k + 4 + 4, y, textBackgroundOpacityFinal << 24);
+			/*
+			MessageIndicator messageIndicator = visible.indicator();
+			if (messageIndicator != null) {
+				int z = messageIndicator.indicatorColor() | u << 24;
+				context.fill(-4, x - lineHeight, -2, x, z);
+				if (s == n && messageIndicator.icon() != null) {
+					int aa = this.getIndicatorX(visible);
+					int ab = y + this.client.textRenderer.fontHeight;
+					this.drawIndicatorIcon(context, aa, ab, messageIndicator.icon());
+				}
+			}
+			*/
+			context.getMatrices().translate(0.0f, 0.0f, 50.0f);
+			context.drawTextWithShadow(this.client.textRenderer, visible.content(), 0, y + lineSpacing, 0xFFFFFF + (chatOpacityFinal << 24));
+			context.getMatrices().pop();
+		}
+		long ac = this.client.getMessageHandler().getUnprocessedMessageCount();
+		if (ac > 0L) {
+			int ad = (int)(128.0 * chatOpacity);
+			ticksAlive = (int)(255.0 * textBackgroundOpacity);
+			context.getMatrices().push();
+			context.getMatrices().translate(0.0f, chatBottomY, 50.0f);
+			context.fill(-2, 0, k + 4, 9, ticksAlive << 24);
+			context.getMatrices().translate(0.0f, 0.0f, 50.0f);
+			context.drawTextWithShadow(this.client.textRenderer, Text.translatable("chat.queue", ac), 0, 1, 0xFFFFFF + (ad << 24));
+			context.getMatrices().pop();
+		}
+		if (isFocused) {
+			int ad = this.getLineHeight();
+			ticksAlive = visibleMessagesCount * ad;
+			int ae = index * ad;
+			int af = this.scrolledLines * ae / visibleMessagesCount - chatBottomY;
+			chatOpacityFinal = ae * ae / ticksAlive;
+			if (ticksAlive != ae) {
+				textBackgroundOpacityFinal = af > 0 ? 170 : 96;
+				w = this.hasUnreadNewMessages ? 0xCC3333 : 0x3333AA;
+				y = k + 4;
+				context.fill(y, -af, y + 2, -af - chatOpacityFinal, w + (textBackgroundOpacityFinal << 24));
+				context.fill(y + 2, -af, y + 1, -af - chatOpacityFinal, 0xCCCCCC + (textBackgroundOpacityFinal << 24));
+			}
+		}
+		context.getMatrices().pop();
+
+
+
+		ci.cancel();
+	}
+}
