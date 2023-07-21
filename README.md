95535f30cf43871aae12519fff2a99cd75dca1fb
Ezzenix <anton.lampinen@gmail.com>
2023-07-21 16:54:57 +0200

    Remove fabric-api

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# ChatAnimations
+# ChatAnimation
 
 This is a Minecraft fabric mod that makes the chat look better by adding animations. It also removes the message indicator.
+
+<img src="src/main/resources/assets/chatanimation/preview.gif"></img>
--- src/main/java/ezzenix/chatanimation/ChatAnimation.java
+++ src/main/java/ezzenix/chatanimation/ChatAnimation.java
@@ -11,10 +11,6 @@ public class ChatAnimation implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("ChatAnimation initialized!");
 	}
 }
