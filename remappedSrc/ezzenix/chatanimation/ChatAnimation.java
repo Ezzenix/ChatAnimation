99a4958e4ea0d481a661787e51f9b3914cde3875
Josh (SupraDev) <51213816+105hua@users.noreply.github.com>
2023-12-18 20:58:16 +0000

    Updated to 1.20.4

---
+++ remappedSrc/ezzenix/chatanimation/ChatAnimation.java
@@ -0,0 +1,16 @@
+package ezzenix.chatanimation;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ChatAnimation implements ModInitializer {
+	public static final String MOD_ID = "chatanimation";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("ChatAnimation initialized!");
+	}
+}
